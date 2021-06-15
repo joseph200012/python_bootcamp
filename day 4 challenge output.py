958591d765c71d6cb149d117e1ba50bdc3a8fad8
joseph200012 <85785975+joseph200012@users.noreply.github.com>
2021-06-15 15:27:31 +0530

    Add files via upload

---
+++ day 4 challenge output.py
@@ -0,0 +1,16 @@
+Python 3.9.5 (tags/v3.9.5:0a7dcbd, May  3 2021, 17:27:52) [MSC v.1928 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+
+2.0
+1000
+80
+DEGRI
+a= 5 b= 3.8
+data type of a : <class 'int'>
+data type of b : <class 'float'>
+value of a and b after type converstion:
+a= 5.0 b= 3
+data type of a after type conversion: <class 'float'>
+data type of b after type conversion: <class 'int'>
+>>> 

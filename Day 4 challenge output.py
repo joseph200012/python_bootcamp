b99fcf58754f72a66b304bb888ba801544e25f0c
joseph200012 <85785975+joseph200012@users.noreply.github.com>
2021-06-15 09:56:42 +0530

    Add files via upload

---
+++ Day 4 challenge output.py
@@ -0,0 +1,11 @@
+Python 3.9.5 (tags/v3.9.5:0a7dcbd, May  3 2021, 17:27:52) [MSC v.1928 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+============ RESTART: C:/Users/VIMALI JOSEPH/Desktop/assignment 4.py ===========
+2.0
+1000
+80
+DEGRI
+a = 3.5
+b = 5
+>>> 

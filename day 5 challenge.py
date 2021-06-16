872c27b4f24fb4067e097fdccac41a105303803f
joseph200012 <85785975+joseph200012@users.noreply.github.com>
2021-06-16 14:15:27 +0530

    Update day 5 challenge.py

--- day 5 challenge.py
+++ day 5 challenge.py
@@ -1,27 +1,28 @@
+# to craete a list and to add, delete, find max , min etc
 print('Write a program to create a list of n integer values and do the following:')
+
 a = []
 
 for i in range(0,51):
 	a.append(i)
-
 print(a)
 
-#•	Add an item in to the list (using function)
+#Add an item in to the list (using function)
 print('Add an item in to the list (using function):')
 a.append(51)
 
-#•	Delete (using function)
+#Delete (using function)
 print('Delete (using function):')
 del a[15]
 
 print(a)
 
-#•	Store the largest number from the list to a variable
+#Store the largest number from the list to a variable
 print('Store the largest number from the list to a variable:')
 max_in_the_list = max(a)
 print(max_in_the_list)
 
-#•	Store the Smallest number from the list to a variable
+#Store the Smallest number from the list to a variable
 print('Store the Smallest number from the list to a variable:')
 min_in_the_list = min(a)
 print(min_in_the_list)

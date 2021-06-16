5de7a206eab0ffb363dda5bdd933788988796956
joseph200012 <85785975+joseph200012@users.noreply.github.com>
2021-06-16 14:12:11 +0530

    Add files via upload

---
+++ day 5 challenge output.py
@@ -0,0 +1,20 @@
+Python 3.9.5 (tags/v3.9.5:0a7dcbd, May  3 2021, 17:27:52) [MSC v.1928 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+========== RESTART: C:/Users/VIMALI JOSEPH/Desktop/day 5 challenge.py ==========
+Write a program to create a list of n integer values and do the following:
+[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50]
+Add an item in to the list (using function):
+Delete (using function):
+[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51]
+Store the largest number from the list to a variable:
+51
+Store the Smallest number from the list to a variable:
+0
+Create a tuple and print the reverse of the created tuple:
+(11, 12, 13, 14, 15)
+(15, 14, 13, 12, 11)
+Create a tuple and convert tuple into list:
+(16, 17, 18, 19, 20)
+[16, 17, 18, 19, 20]
+>>> 

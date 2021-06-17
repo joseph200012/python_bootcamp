1b0a525e7c575c5afd8d4c85d6ed3feeb063e318
joseph200012 <85785975+joseph200012@users.noreply.github.com>
2021-06-17 12:12:12 +0530

    Add files via upload

---
+++ day 6 challenge output.py
@@ -0,0 +1,17 @@
+Python 3.9.5 (tags/v3.9.5:0a7dcbd, May  3 2021, 17:27:52) [MSC v.1928 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+
+{'j': 1, 'l': 2, 'h': 3, 'f': 4}
+Dictionaries before removal : {'raju': 75, 'dannish': 70, 'louis': 60, 'babu': 95}
+Dictionaries after removal : {'raju': 75, 'dannish': 70, 'louis': 60}
+ mapping two list into dictionaries : {'beetroot': '60', 'tomato': '85', 'onion': '20', 'carrot': '120'}
+The length of the set is : 5
+Current set :
+{'3', '4', '2', '1', '5'}
+{'2', '9', '1', '7', '5'}
+
+ remove the intersection of 2nd set from 1 st set using -= operator :
+set1 {'3', '4'}
+set2 {'2', '9', '1', '7', '5'}
+>>> 

3ab2fdc7ab72bfd832ebf2439dba84f2483a8cba
joseph200012 <85785975+joseph200012@users.noreply.github.com>
2021-06-18 12:36:49 +0530

    Add files via upload

---
+++ day 7 challenge output.py
@@ -0,0 +1,16 @@
+Python 3.9.5 (tags/v3.9.5:0a7dcbd, May  3 2021, 17:27:52) [MSC v.1928 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+enter the first number :4
+enter the second number :10
+Addition of two number is 14
+Subtraction of two number is -6
+Division of two number is 0.4
+Multiplication of two number is 40
+
+Enter the Patient's name : Louis
+Enter the Patient's body temperature in degree : 97
+Patient's Details: 
+Patient's name : Louis
+Default temperature : 98 degree  Patient's body temperature : 97 degree
+>>> 

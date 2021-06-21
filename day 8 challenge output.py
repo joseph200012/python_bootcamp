ab02aa85c833a9189bcce9adf153813eee455a57
joseph200012 <85785975+joseph200012@users.noreply.github.com>
2021-06-21 07:34:01 +0530

    Add files via upload

---
+++ day 8 challenge output.py
@@ -0,0 +1,45 @@
+Python 3.9.5 (tags/v3.9.5:0a7dcbd, May  3 2021, 17:27:52) [MSC v.1928 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+
+{'a': 15, 'b': 20, 'x': 25, 'y': 30}
+['1', '2', '3', '4', '5']
+['5', '4', '3', '2', '1']
+{'1', '4', '3', '5', '2'}
+dict_keys(['key1', 'key2', 'key3'])
+
+
+key1
+key2
+key3
+ Hi World
+enter the word to search: Hi
+enter the word to replace: Hello
+ Hello World
+ i am dani
+enter the word to be find: i
+ I am danI
+{'one': 1, 'two': 2, 'three': 1, 'four': 1, 'five': 2}
+enter three value to be added
+operand_1=15
+operand_2=51
+operand_3=7
+operand_1+operand_2+operand_3 = 73
+enter the divisor=8
+your final answer is 9.125
+operand_1=36
+operand_2=49
+operand_3=64
+mean is  49.666666666666664
+median is  49
+mode is  64
+this is a demo class
+THIS IS A DEMO CLASS
+1
+0
+0
+0
+1
+0
+0
+>>> 

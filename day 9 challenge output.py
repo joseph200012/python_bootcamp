4f3f5e263c92b689742a9140a3020a6662c7084a
joseph200012 <85785975+joseph200012@users.noreply.github.com>
2021-06-21 23:16:07 +0530

    Add files via upload

---
+++ day 9 challenge output.py
@@ -0,0 +1,42 @@
+Python 3.9.5 (tags/v3.9.5:0a7dcbd, May  3 2021, 17:27:52) [MSC v.1928 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+
+[3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+5 4 3 2 1 
+4 3 2 1 
+3 2 1 
+2 1 
+1 
+Enter a number: 160
+160 is not an Armstrong number
+9 x  1  =  9
+9 x  2  =  18
+9 x  3  =  27
+9 x  4  =  36
+9 x  5  =  45
+9 x  6  =  54
+9 x  7  =  63
+9 x  8  =  72
+9 x  9  =  81
+9 x  10  =  90
+-1  is negative 
+2  is positive
+-4  is negative 
+5  is positive
+-9  is negative 
+87  is positive
+1 Years ago !
+4 Years ago !
++
+-
+*
+/
+%
+**
+Select an operator:n/
+Enter two numbers
+6
+3
+2.0
+>>>

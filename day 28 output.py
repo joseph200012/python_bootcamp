d0a4998f9172a3d8e3edaceb7e546d583315c12b
joseph200012 <85785975+joseph200012@users.noreply.github.com>
2021-07-19 16:23:36 +0530

    Add files via upload

---
+++ day 28 output.py
@@ -0,0 +1,9 @@
+Python 3.9.5 (tags/v3.9.5:0a7dcbd, May  3 2021, 17:27:52) [MSC v.1928 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+
+2021-07-19 16:19:38.511994
+number of active threads : 2021-07-19 16:19:38.511994
+ 
+4
+>>> 
